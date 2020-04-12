740f3249aded0ff5e3b0ab7e79ef2ea67a2bebac
Robert Laverty <roblav96@gmail.com>
2020-04-12 05:22:37 -0400

    v0.0.1

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
-import * as brews from '.'
-import * as fs from 'fs-extra'
+import * as bbrew from '.'
 import * as mri from 'mri'
-import * as path from 'path'
 
 process.nextTick(async () => {
 	let argvs = mri(process.argv.slice(2))
-
 	let inputs = argvs._ as string[]
 })
