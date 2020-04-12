4ce3056aaca280aee4a59ad5555c762f6f1bb5bf
Robert Laverty <roblav96@gmail.com>
2020-04-12 06:09:23 -0400

    init oclif

--- src/cli.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import * as bbrew from '.'
-import * as mri from 'mri'
-
-process.nextTick(async () => {
-	let argvs = mri(process.argv.slice(2))
-	let inputs = argvs._ as string[]
-})
